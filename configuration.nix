20a8fa6e22b0a2e286c667b3e2c45f07a689467c
Ryan Gillespie <ryan.gillespie@achievers.com>
2025-07-25 14:27:54 -0400

    first commit

---
+++ configuration.nix
@@ -0,0 +1,56 @@
+{ config, pkgs, ... }:
+{
+  imports = [ ./hardware-configuration.nix ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "macbook"; 
+  networking.networkmanager.enable = true;
+
+  time.timeZone = "America/Toronto";
+  i18n.defaultLocale = "en_CA.UTF-8";
+  
+  services.xserver.videoDrivers = [ "modesetting" ];
+  services.libinput.enable = true;
+  services.libinput.touchpad.naturalScrolling = true;
+
+  programs.fish.enable = true;
+  users.users.ryan = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "networkmanager" ];
+    shell = pkgs.fish;
+  };
+
+  programs.hyprland.enable = true;
+  xdg.portal.enable = true;
+  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+
+  environment.systemPackages = with pkgs; [
+    hyprland hyprpaper hyprpanel
+    rofi-wayland nautilus ghostty zen-browser obsidian
+    vscode htop btop onepassword lazyvim llm-studio
+    fish fontconfig gtk3 gtk4 mononoki wget curl git unzip
+  ];
+
+  fonts = {
+    enableDefaultPackages = true;
+    packages = with pkgs; [ (nerdfonts.override { fonts = [ "Mononoki" ]; }) ];
+  };
+
+  programs.dconf.enable = true;
+  environment.variables = {
+    GTK_THEME = "Ayu-Mirage";
+  };
+
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  services.pipewire = {
+    enable = true;
+    pulse.enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+  };
+
+  nixpkgs.config.allowUnfree = true;
+}
---
+++ home.nix
@@ -0,0 +1,48 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "ryan";
+  home.homeDirectory = "/home/ryan";
+
+  programs.vscode.enable = true;
+
+  programs.fish = {
+    enable = true;
+    shellAliases = {
+      ll = "ls -alh";
+      update = "sudo nixos-rebuild switch --flake ~/nixos-config#macbook";
+    };
+  };
+
+  programs.neovim = {
+    enable = true;
+    extraConfig = ''
+      set number
+      set relativenumber!
+    '';
+  };
+
+  gtk = {
+    enable = true;
+    theme.name = "Ayu-Mirage";
+    iconTheme.name = "Papirus-Dark";
+    cursorTheme.name = "Adwaita";
+    font.name = "Mononoki Nerd Font Regular 11";
+  };
+
+  home.file.".config/hypr/hyprland.conf".text = ''
+    monitor=,preferred,auto,1
+    exec-once = rofi-wayland
+    exec-once = hyprpanel
+  '';
+
+  home.file.".config/ghostty/config".text = ''
+    font-family = Mononoki Nerd Font Regular
+    font-family-bold = Mononoki Nerd Font Regular
+    font-family-italic = Mononoki Nerd Font Regular
+    font-family-bold-italic = Mononoki Nerd Font Regular
+    theme = Ayu Mirage
+  '';
+
+  home.stateVersion = "24.05";
+}
